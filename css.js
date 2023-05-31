b4a127222afc10a2fbaee5e499bea16ac542b257
AsrielPires <bebether@gmail.com>
2023-06-01 00:55:11 +0100

    general

--- css.js
+++ css.js
@@ -1,4 +1,28 @@
 export const bookCss = () => ({
+    // "._.sheet": {
+    //   overflow: "hidden",
+    //   position: "relative",
+    //   breakInside: "avoid",
+    //   cursor: "text",
+    //   whiteSpace: "pre-wrap",
+    //   background: "#fff",
+    //   color: "#000",
+    //   userSelect: "text",
+    //   ":not(:first-child)": { breakBefore: "page" },
+    // },
+    // "._.wm": {
+    //   userSelect: "none",
+    //   position: "absolute",
+    //   fontSize: "120pt",
+    //   margin: "auto",
+    //   transform: "translate(-50%, -50%)rotate(-45deg)",
+    //   top: "50%",
+    //   left: "50%",
+    //   whiteSpace: "nowrap",
+    //   opacity: 0.3,
+    //   "&.v": { transform: "translate(-50%, -50%)rotate(-65deg)" },
+    //   "&.h": { transform: "translate(-50%, -50%)rotate(-30deg)" },
+    // },
     "@media screen": {
         "._.sheet": {
             margin: "0 auto",
@@ -6,5 +30,10 @@ export const bookCss = () => ({
             marginBottom: "6px",
         }
     },
+    // "@media print": {
+    //   ":not(._.sheet)": {
+    //     display: "none"
+    //   },
+    // }
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3NzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY3NzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUVBLE1BQU0sQ0FBQyxNQUFNLE9BQU8sR0FBRyxHQUFXLEVBQUUsQ0FBQyxDQUFDO0lBeUJwQyxlQUFlLEVBQUU7UUFDZixVQUFVLEVBQUU7WUFDVixNQUFNLEVBQUUsUUFBUTtZQUNoQixTQUFTLEVBQUUscUJBQXFCO1lBQ2hDLFlBQVksRUFBRSxLQUFLO1NBQ3BCO0tBQ0Y7Q0FNRixDQUFDLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3NzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY3NzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUVBLE1BQU0sQ0FBQyxNQUFNLE9BQU8sR0FBRyxHQUFXLEVBQUUsQ0FBQyxDQUFDO0lBQ3BDLGdCQUFnQjtJQUNoQix3QkFBd0I7SUFDeEIsMEJBQTBCO0lBQzFCLDBCQUEwQjtJQUMxQixvQkFBb0I7SUFDcEIsNEJBQTRCO0lBQzVCLHdCQUF3QjtJQUN4QixtQkFBbUI7SUFDbkIsd0JBQXdCO0lBQ3hCLG1EQUFtRDtJQUNuRCxLQUFLO0lBQ0wsYUFBYTtJQUNiLHdCQUF3QjtJQUN4QiwwQkFBMEI7SUFDMUIsdUJBQXVCO0lBQ3ZCLG9CQUFvQjtJQUNwQixzREFBc0Q7SUFDdEQsZ0JBQWdCO0lBQ2hCLGlCQUFpQjtJQUNqQiwwQkFBMEI7SUFDMUIsa0JBQWtCO0lBQ2xCLGlFQUFpRTtJQUNqRSxpRUFBaUU7SUFDakUsS0FBSztJQUNMLGVBQWUsRUFBRTtRQUNmLFVBQVUsRUFBRTtZQUNWLE1BQU0sRUFBRSxRQUFRO1lBQ2hCLFNBQVMsRUFBRSxxQkFBcUI7WUFDaEMsWUFBWSxFQUFFLEtBQUs7U0FDcEI7S0FDRjtJQUNELG9CQUFvQjtJQUNwQix3QkFBd0I7SUFDeEIsc0JBQXNCO0lBQ3RCLE9BQU87SUFDUCxJQUFJO0NBQ0wsQ0FBQyxDQUFDIn0=
